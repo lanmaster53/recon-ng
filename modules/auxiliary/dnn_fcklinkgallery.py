77ec823137bc8336fc1375682e2e90660c6952f4
Jay <shipcodez@gmail.com>
2013-02-07 20:56:08 +0800

    made some minor fixes

--- modules/auxiliary/dnn_fcklinkgallery.py
+++ modules/auxiliary/dnn_fcklinkgallery.py
@@ -1,20 +1,19 @@
 import framework
 # unique to module
-import os
 
 class Module(framework.module):
-
-    def __init__(self, params):
+	
+	def __init__(self, params):
         framework.module.__init__(self, params)
-        self.register_option('source', 'database', 'yes', 'source of module input')
+        self.register_option('source', 'db', 'yes', 'source of module input')
         self.register_option('verbose', self.goptions['verbose']['value'], 'yes', self.goptions['verbose']['desc'])
         self.info = {
                      'Name': 'Dot Net Nuke Remote File Upload Vulnerability Checker',
                      'Author': 'Jay Turla (@shipcod3)',
                      'Description': 'Checks the hosts for a DNN fcklinkgallery page which is possibly vulnerable to Remote File Upload.',
                      'Comments': [
-                                  'Source options: database, <hostname>, <path/to/infile>',
-                                  'http://www.exploit-db.com/exploits/12700/'
+                                  'Source options: db, <hostname>, <path/to/infile>',
+                                  'http://www.exploit-db.com/exploits/12700/',
                                   ]
                      }
 
@@ -26,15 +25,8 @@ class Module(framework.module):
     def check_for_dnnfcklink(self):
         verbose = self.options['verbose']['value']
         
-        # handle sources
-        source = self.options['source']['value']
-        if source == 'database':
-            hosts = [x[0] for x in self.query('SELECT DISTINCT host FROM hosts WHERE host IS NOT NULL ORDER BY host')]
-            if len(hosts) == 0:
-                self.error('No hosts in the database.')
-                return
-        elif os.path.exists(source): hosts = open(source).read().split()
-        else: hosts = [source]
+        hosts = self.get_source(self.options['source']['value'], 'SELECT DISTINCT host FROM hosts WHERE host IS NOT NULL ORDER BY host')
+        if not hosts: return
 
         # check all hosts for DNN fcklinkgallery page
         protocols = ['http', 'https']
@@ -51,8 +43,8 @@ class Module(framework.module):
                 except:
                    code = 'Error'
                 if code == 200 and '> Link Gallery' in resp.text:
-                    self.alert('%s. > Possible DNN Fcklinkgallery page found!' % (url))
+                    self.alert('%s. => Possible DNN Fcklinkgallery page found!' % (url))
                     cnt += 1
                 else:
-                    if verbose: self.output('%s => %s Error!' % (url, code))
+                    if verbose: self.output('%s => %s' % (url, code))
         self.output('%d DNN Fcklinkgallery pages found.' % (cnt))
