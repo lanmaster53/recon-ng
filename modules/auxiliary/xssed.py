2f65e8c9dfb54775fb54f3c2379d1f1a1a1a5ab8
WebBreacher <webbreacher@gmail.com>
2013-02-10 14:26:09 -0500

    Used framework's url requestor and made output in table. Had to wrap long URLs too

--- modules/auxiliary/xssed.py
+++ modules/auxiliary/xssed.py
@@ -1,7 +1,6 @@
 import framework
 # unique to module
 import re
-import urllib2
 
 class Module(framework.module):
 
@@ -12,10 +11,10 @@ class Module(framework.module):
         self.info = {
                      'Name': 'XSSed Host Lookup',
                      'Author': 'Micah Hoffman (@WebBreacher)',
-                     'Description': 'Checks XSSed site for XSS records for given target and displays first 20 hits.',
+                     'Description': 'Checks XSSed.com site for XSS records for given domain and displays first 20 hits.',
                      'Comments': []
                      }
-
+   
     def do_run(self, params):
         if not self.validate_options(): return
         # === begin here ===
@@ -27,25 +26,27 @@ class Module(framework.module):
 
         url = 'http://xssed.com/search?key=%s' % (domain)
         if verbose: self.output('URL for XSSED.com: %s' % url)
-        try: resp = urllib2.urlopen(url)
+        try: resp = self.request(url)
         except KeyboardInterrupt:
             print ''
         except Exception as e:
             self.error(e.__str__())
             return
         
-        content = resp.read()
+        content = resp.text
 
         # Find if there are any results for the domain search
         results = re.findall(r"Results for.*", content)
+       
         if results:
             rows = re.split('<br>', str(results))
             for row in rows:
                 finding = re.findall(r"mirror/([0-9]+)/.+blank\\'>(.+?)</a>", row)
                 if finding:
-                    # Now go fetch and parse the specific page for this item
+                
+                    # Go fetch and parse the specific page for this item
                     urlDetail = 'http://xssed.com/mirror/%s/' % finding[0][0]
-                    try: respDetail = urllib2.urlopen(urlDetail)
+                    try: respDetail = self.request(urlDetail)
                     except KeyboardInterrupt:
                         print ''
                     except Exception as e:
@@ -54,21 +55,46 @@ class Module(framework.module):
                     
                     # Parse the response and get the details
                     details = []
-                    for line in respDetail.readlines():
+                    for line in respDetail.text.split('\n'):
                         if "row3" in line:
                             try: 
                                 a = re.search(r'">(.+)</th', line.strip())
                                 details.append(a.group(1))
                             except: pass
-                    # Output the results
+                            
+                    # Output the results in table format
                     status = re.search(r';([UNFIXED]+)$',details[2])
-                    self.output(details[4])
-                    self.output('  '+details[7])
-                    self.output('  '+details[0].replace('&nbsp;', ' '))
-                    self.output('  '+details[1].replace('&nbsp;', ' '))
-                    self.output('  '+details[5])
-                    self.output('  STATUS: '+status.group(1))
-                    self.error(' ')                    
+                                          
+                    tdata = [] 
+                    tdata.append(['Category', 'Details Retrieved'])
+                    tdata.append(details[4].split(":",1))                           # Domain
+                    
+                    # Line wrapping for long XSS URLs that break table formatting
+                    urlMaxLen = 80
+                    xssUrlLen = len(details[7].split(":",1)[1])
+                    if xssUrlLen > urlMaxLen:
+                        wrappedLines = []
+                        for line in details[7].split(":",1)[1].strip().split('\n'):
+                            while True:
+                                wrappedLines.append(line[:urlMaxLen])
+                                line = line[urlMaxLen:]
+                                if not line: break
+                        counter = 1
+                        for item in wrappedLines:
+                            if counter == 1:
+                                tdata.append(['URL:', ' ' + item])
+                            else:
+                                tdata.append(["URL (con't):", '   ' + item])
+                            counter += 1
+                    else:
+                        tdata.append(details[7].split(":",1))                       # URL
+                        
+                    tdata.append(details[0].replace('&nbsp;', ' ').split(":",1))    # Date submitted
+                    tdata.append(details[1].replace('&nbsp;', ' ').split(":",1))    # Date Published
+                    tdata.append(details[5].split(":",1))                           # Category
+                    tdata.append(['STATUS', ' ' + status.group(1)])                 # Fixed
+                    self.table(tdata, True)   
+                        
         else:
             self.output('No results found')
         
