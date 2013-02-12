0bca2c86ce71ccec6906579601754fd4e484ee1f
WebBreacher <webbreacher@gmail.com>
2013-02-11 22:07:11 -0500

    Adding new modules

--- modules/auxiliary/urlvoid.py
+++ modules/auxiliary/urlvoid.py
@@ -1,5 +1,6 @@
 import framework
 # unique to module
+import re
 
 class Module(framework.module):
 
@@ -7,23 +8,24 @@ class Module(framework.module):
         framework.module.__init__(self, params)
         self.register_option('domain', self.goptions['domain']['value'], 'yes', self.goptions['domain']['desc'])
         self.register_option('verbose', self.goptions['verbose']['value'], 'yes', self.goptions['verbose']['desc'])
+        self.classify = 'passive'
         self.info = {
-                     'Name': 'McAfee Mail Host Lookup',
+                     'Name': 'URLVoid Domain Lookup',
                      'Author': 'Micah Hoffman (@WebBreacher)',
-                     'Description': 'Checks mcafee.com site for mail servers for given domain.',
+                     'Description': 'Checks urlvoid.com site for information about the security of a domain.',
                      'Comments': []
                      }
    
     def do_run(self, params):
         if not self.validate_options(): return
         # === begin here ===
-        self.mcafee_mail()
+        self.urlvoid()
 
-    def mcafee_mail(self):
+    def urlvoid(self):
         verbose = self.options['verbose']['value']
         domain = self.options['domain']['value']
 
-        url = 'http://www.mcafee.com/threat-intelligence/jsproxy/domain.ashx?q=mail&f=%s' % (domain)
+        url = 'http://www.urlvoid.com/scan/%s/' % (domain)
         if verbose: self.output('URL being retrieved: %s' % url)
         try: resp = self.request(url)
         except KeyboardInterrupt:
@@ -33,17 +35,14 @@ class Module(framework.module):
             return
 
         if resp:
-            # Output the results in table format
-            tdata = [] 
-            tdata.append(['Domain', 'MX_Data', 'IP_Address', 'Weight'])
-            for col in resp.json['data']:
-                tdata.append([col['Domain'], col['MX_Data'], col['IP_Address'], col['Weight']])
-                
-                # Add each host to the database
-                self.add_host(col['MX_Data'], address=col['IP_Address'])
-                
-            # Print the table            
+            # Get the security results
+            av_engines = re.findall(r'<td>(.+)</td>\n.*images/(.+)\.png" alt=""', resp.text)
+            tdata = []
+            tdata.append(['Site', 'Status'])
+            for line in av_engines:
+                tdata.append([line[0], line[1]])
             self.table(tdata, True)
 
         else:
             self.output('No results found')
+        
