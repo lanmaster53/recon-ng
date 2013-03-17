d477c1bec5275cf410cd8941716e57d00802ee9f
root <root@localhost.localdomain>
2013-03-16 22:14:21 -0400

    Polished it up. Ready for pull.

--- modules/recon/hosts/enum/http/ipvoid.py
+++ modules/recon/hosts/enum/http/ipvoid.py
@@ -28,41 +28,29 @@ class Module(framework.module):
             return
 
         # Get and display the results
-        self.heading('Results for %s' % ip, 1)
+        self.heading('Information about %s' % ip, 1)
         blacklisted = re.search(r'Blacklist Status</td><td><span.+>(\w.+)</span>', resp.text)
         if blacklisted.group(1) == "BLACKLISTED":
-            self.alert('Status: %s' % blacklisted.group(1))
+            self.alert('This IP is BLACKLISTED! (ruhroh)')
+            detection = re.search(r'Detection Ratio</td><td>(\d+ / \d+) \(<font', resp.text)
+            self.alert('Detection Ratio was %s' % detection.group(1))
             detected_line = re.search(r'\s+<tr><td><img src="(.+)', resp.text)
-            detected_sites = re.findall(r'Favicon" />(.+?)</td><td><img src=".+?" alt="Alert" title="Detected!" ', detected_line.group(1))
+            detected_sites = re.findall(r'Favicon" />(.+?)</td><td><img src=".+?" alt="Alert" title="Detected!".+?"nofollow" href="(.+?)" title', detected_line.group(1))
+            tdata = []
+            tdata.append(['Site', 'Status'])
             for site in detected_sites:
-                self.alert(site)
+                tdata.append([site[0], site[1]])
+            self.table(tdata, True)
+        else:
+            self.output('Not blacklisted...whew!')
         
-        detection = re.search(r'Detection Ratio</td><td>(\d+ / \d+) \(<font', resp.text)
+        # Other Data scraped from the page
         reverse_dns = re.search(r'Reverse DNS</td><td>(.+)</td>', resp.text)
         country = re.search(r'alt="Flag" /> \(\w+\) (.+)</td>', resp.text)
         city = re.search(r'City</td><td>(\w.+)</td>', resp.text)
         region = re.search(r'Region</td><td>(\w.+)</td>', resp.text)
         lat_long = re.search(r'Latitude / Longitude</td><td>(.+)</td>',resp.text)
+        # Output that additional stuff above
+        self.output('Reverse DNS entry: %s' % (reverse_dns.group(1)))
+        self.output('Location: %s, %s, %s at %s' % (country.group(1).strip(), region.group(1).strip(), city.group(1).strip(), lat_long.group(1).strip()))
         
-        
-        
-        detected_sites = re.findall(r'Favicon" />(.+?)</td><td><img src=".+" alt="Alert" title="Detected!" ', resp.text)
-        
-        
-        # Output
-        
-        self.output('Status: %s' % blacklisted.group(1))
-        
-        if detected_sites:
-            for site in detected_sites:
-                self.output(site)
-        else:
-            "Not currently blacklisted on any sites"
-                
-        
-        
-        '''tdata = []
-        tdata.append(['Site', 'Status'])
-        for line in av_engines:
-            tdata.append([line[0], line[1]])
-        self.table(tdata, True)'''
