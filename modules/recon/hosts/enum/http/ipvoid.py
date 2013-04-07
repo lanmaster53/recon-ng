eaead85a55d7ad58dd616f3db359d069caaf9da4
WebBreacher <webbreacher@gmail.com>
2013-04-07 10:01:13 -0400

    Modded per lanmaster53's feedback on the pull

--- modules/recon/hosts/enum/http/ipvoid.py
+++ modules/recon/hosts/enum/http/ipvoid.py
@@ -6,7 +6,7 @@ class Module(framework.module):
 
     def __init__(self, params):
         framework.module.__init__(self, params)
-        self.register_option('ip', False, 'yes', 'Enter the IP you would like to search for')
+        self.register_option('ip', None, 'yes', 'Enter the IP you would like to search for')
         self.info = {
                      'Name': 'IPVoid IP Address Lookup',
                      'Author': 'Micah Hoffman (@WebBreacher)',
@@ -28,7 +28,7 @@ class Module(framework.module):
             return
 
         # Get and display the results
-        self.heading('Information about %s' % ip, 1)
+        self.heading('Information about %s' % ip)
         blacklisted = re.search(r'Blacklist Status</td><td><span.+>(\w.+)</span>', resp.text)
         if blacklisted.group(1) == "BLACKLISTED":
             self.alert('This IP is BLACKLISTED! (ruhroh)')
@@ -36,8 +36,7 @@ class Module(framework.module):
             self.alert('Detection Ratio was %s' % detection.group(1))
             detected_line = re.search(r'\s+<tr><td><img src="(.+)', resp.text)
             detected_sites = re.findall(r'Favicon" />(.+?)</td><td><img src=".+?" alt="Alert" title="Detected!".+?"nofollow" href="(.+?)" title', detected_line.group(1))
-            tdata = []
-            tdata.append(['Site', 'Status'])
+            tdata = [['Site', 'Status']]
             for site in detected_sites:
                 tdata.append([site[0], site[1]])
             self.table(tdata, True)
@@ -46,11 +45,11 @@ class Module(framework.module):
         
         # Other Data scraped from the page
         reverse_dns = re.search(r'Reverse DNS</td><td>(.+)</td>', resp.text)
-        country = re.search(r'alt="Flag" /> \(\w+\) (.+)</td>', resp.text)
         city = re.search(r'City</td><td>(\w.+)</td>', resp.text)
         region = re.search(r'Region</td><td>(\w.+)</td>', resp.text)
+        country = re.search(r'alt="Flag" /> \(\w+\) (.+)</td>', resp.text)
         lat_long = re.search(r'Latitude / Longitude</td><td>(.+)</td>',resp.text)
         # Output that additional stuff above
         self.output('Reverse DNS entry: %s' % (reverse_dns.group(1)))
-        self.output('Location: %s, %s, %s at %s' % (country.group(1).strip(), region.group(1).strip(), city.group(1).strip(), lat_long.group(1).strip()))
+        self.output('Location: %s, %s, %s at %s' % (city.group(1).strip(), region.group(1).strip(), country.group(1).strip(), lat_long.group(1).strip()))
         
