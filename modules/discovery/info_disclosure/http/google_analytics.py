ee64dbc73f614c9abdb4f33829ac29563a7a8afc
WebBreacher <webbreacher@gmail.com>
2013-06-05 21:56:14 -0400

    Added capability for AdSense code

--- modules/discovery/info_disclosure/http/google_analytics.py
+++ modules/discovery/info_disclosure/http/google_analytics.py
@@ -10,30 +10,40 @@ class Module(framework.module):
         self.info = {
                      'Name': 'Google Analytics Host Lookup',
                      'Author': 'Micah Hoffman (@WebBreacher)',
-                     'Description': 'Provide this module with a URL (or domain) and it will visit that page and look for a Google Analytics "UA-#####" number in the page source. Then it will look up other sites that use that same UA-###### code. This can show you sites that may be administered or coded by a single group.',
+                     'Description': 'Provide this module with a URL (or domain) and it will visit that page and look for a Google Analytics "UA-#####" and Google AdSense ID "pub-####" number in the page source. Then it will look up other sites that use that same code. This can show you sites that may be administered or coded by a single group.',
                      'Comments': []
                      }
+                     
+    def lookup_ewhois(self, code):
+        self.alert('Found Code: %s' % code)
+        # Now go look up the code in the ewhois site and scrape results
+        if code.startswith("UA"):
+        	ewhois_url = 'http://www.ewhois.com/analytics-id/%s/' % code ### Analytics
+        else:
+        	ewhois_url = 'http://www.ewhois.com/adsense-id/%s/' % code   ### AdSense
+        self.verbose('Searching %s for other domains' % ewhois_url)
+        ewhois_resp = self.request(ewhois_url)
+        ewhois_content = ewhois_resp.text
+        return re.findall('<div class="row"><a[^>]*>(.+?)</a>', ewhois_content)
    
     def module_run(self):
-        # Visit Target URL and scrape for UA- code
+    	results_ana = results_ad = set()
+        # Visit Target URL and scrape for codes
         url_target_site = self.options['url']['value']
         if re.search('^https*://', url_target_site):
             self.verbose('Retrieving source for: %s' % url_target_site)
             resp = self.request(url_target_site)
             content = resp.text
-            code = re.search('["\'](UA-\d+)', content)
-            if code:
-                self.alert('Found Analytics Code: %s' % code.group(1))
-                # Now go look up the code in the ewhois site and scrape results
-                ewhois_url = 'http://www.ewhois.com/analytics-id/%s/' % code.group(1)
-                self.verbose('Searching %s for other domains' % ewhois_url)
-                ewhois_resp = self.request(ewhois_url)
-                ewhois_content = ewhois_resp.text
-                domains = re.findall('<div class="row"><a[^>]*>(.+?)</a>', ewhois_content)
-                domains.sort()
-                for domain in domains:
-                    self.alert(domain)        
+            
+            code_ana = re.search('["\'](UA-\d+)', content) ### Analytics ID
+            code_ad  = re.search('["\'](pub-\d+)', content) ### AdSense ID
+            if code_ana: results_ana = set(self.lookup_ewhois(code_ana.group(1)))
+            if code_ad:  results_ad  = set(self.lookup_ewhois(code_ad.group(1)))
+            if not code_ana and not code_ad:
+                self.output('No Google Analytics or Google AdSense code found in target URL source.')
             else:
-                self.output('No Google Analytics code found in target URL source.')
+            	total_results = results_ana.union(results_ad)
+            	for domain in sorted(total_results):
+            		self.alert(domain)
         else:
             self.output('The URL parameter (%s) is not a recognized URL. Please re-enter one in the "http[s]://example.com" format.' % url_target_site)
