5ca87e0b25241c1684badaa23aff3854c9b9bd21
Benjamin Donnelly <accipitercaelum@gmail.com>
2013-11-09 17:32:00 +0000

    Edited the patch again

--- modules/reporting/pushpin.py
+++ modules/reporting/pushpin.py
@@ -38,10 +38,10 @@ class Module(framework.module):
             items = self.query('SELECT * FROM pushpin WHERE source=?', (source,))
             items.sort(key=lambda x: x[9], reverse=True)
             for item in items:
-                item = [self.to_unicode_str(x) if x != None and x != "\n" else u'' for x in item]
+                item = [self.to_unicode_str(x) if x != None else u'' for x in item]
                 media_content += '<div class="media_row"><div class="prof_cell"><a href="%s" target="_blank"><img class="prof_img" src="%s" /></a></div><div class="data_cell"><div class="trigger" id="trigger" lat="%s" lon="%s">[<a href="%s" target="_blank">%s</a>] %s<br /><span class="time">%s</span></div></div></div>\n' % (item[4], item[5], item[7], item[8], item[3], item[2], re.sub('[\r\n]+', '<br />', self.html_escape(item[6])), item[9])
-                map_details = "<table><tr><td class='prof_cell'><a href='%s' target='_blank'><img class='prof_img' src='%s' /></a></td><td class='data_cell'>[<a href='%s' target='_blank'>%s</a>] %s<br /><span class='time'>%s</span></td></tr></table>" % (item[4], item[5], item[3], item[2], re.sub('[\r\n]+', '<br />', self.html_escape(item[6])), item[9])
-                map_content += '\t\tadd_marker({position: new google.maps.LatLng(%s,%s),title:"%s",icon:"%s",map:map},{details:"%s"});\n' % (item[7], item[8], item[2], icons[source.lower()], map_details)
+                map_details = "<table><tr><td class='prof_cell'><a href='%s' target='_blank'><img class='prof_img' src='%s' /></a></td><td class='data_cell'>[<a href='%s' target='_blank'>%s</a>] %s<br /><span class='time'>%s</span></td></tr></table>" % (item[4], item[5], item[3], re.sub("\n", "", item[2]), re.sub('[\r\n]+', '<br />', self.html_escape(item[6])), item[9])
+                map_content += '\t\tadd_marker({position: new google.maps.LatLng(%s,%s),title:"%s",icon:"%s",map:map},{details:"%s"});\n' % (item[7], item[8], re.sub("\n", "", item[2]), icons[source.lower()], map_details)
             media_content += '</div>\n'
         return media_content, map_content
 
