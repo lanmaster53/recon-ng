c1ca4a214546549b0aaccead3d7d44cca4e36c07
Quentin Kaiser <kaiserquentin@gmail.com>
2013-12-17 17:02:00 +0100

    Add domain option

--- modules/recon/hosts/gather/http/web/vpnhunter.py
+++ modules/recon/hosts/gather/http/web/vpnhunter.py
@@ -5,6 +5,7 @@ import time
 class Module(framework.module):
     def __init__(self, params):
         framework.module.__init__(self, params)
+        self.register_option('domain', self.goptions['domain']['value'], 'yes', 'domain to check for remote accesses')
         self.info = {
             'Name': 'VPNHunter Lookup',
             'Author': 'Quentin Kaiser (contact[at]quentinkaiser.be)',
@@ -15,6 +16,7 @@ class Module(framework.module):
 
     def module_run(self):
 
+        domain = self.options['domain']['value']
         self.services = {
             'sslvpn': {'pretty': 'SSL VPN'},
             'remoteaccess': {'pretty': 'remote access'},
@@ -22,46 +24,43 @@ class Module(framework.module):
             'genericlogin': {'pretty': 'generic login page'}
         }
 
-        if not len(self.goptions['domain']['value']):
-            self.error('The domain value is empty')
+
+        payload = {'fqdn': domain}
+        headers = {
+            'Host': 'www.vpnhunter.com',
+            'Origin': 'http://www.vpnhunter.com/',
+            'Referer': 'http://www.vpnhunter.com/'
+        }
+        resp = self.request(
+            'http://www.vpnhunter.com/',
+            method='POST',
+            headers=headers,
+            payload=payload,
+            redirect=False
+        )
+        if resp.status_code != 302:
+            self.error("An error occured while requesting vpnhunter.com")
             return
-        else:
-            payload = {'fqdn': self.goptions['domain']['value']}
-            headers = {
-                'Host': 'www.vpnhunter.com',
-                'Origin': 'http://www.vpnhunter.com/',
-                'Referer': 'http://www.vpnhunter.com/'
-            }
-            resp = self.request(
-                'http://www.vpnhunter.com/',
-                method='POST',
-                headers=headers,
-                payload=payload,
-                redirect=False
-            )
-            if resp.status_code != 302:
+
+        hash = resp.headers['location'].replace("/r/", "")
+
+        headers['Referer'] = "http://www.vpnhunter.com/r/%s" % (hash)
+        payload['hash'] = hash
+
+        for service in self.services:
+            payload['type'] = service
+            self.output("Checking for %s on %s" % (self.services[service]['pretty'], payload['fqdn']))
+            resp = self.request('http://www.vpnhunter.com/poll', headers=headers, payload=payload)
+            if resp.status_code == 200:
+                content = resp.json
+                if len(content["result"]):
+                    for result in content["result"]:
+                        if len(result['address']) == 2:
+                            self.alert("Found 1 %s running %s on %s (port %s)" %
+                                       (self.services[service]['pretty'], result['vendor'], result['address'][0],
+                                        result['address'][1]))
+                        else:
+                            self.alert("Found 1 %s running %s on %s" %
+                                       (self.services[service]['pretty'], result['vendor'], result['address']))
+            else:
                 self.error("An error occured while requesting vpnhunter.com")
-                return
-
-            hash = resp.headers['location'].replace("/r/", "")
-
-            headers['Referer'] = "http://www.vpnhunter.com/r/%s" % (hash)
-            payload['hash'] = hash
-
-            for service in self.services:
-                payload['type'] = service
-                self.output("Checking for %s on %s" % (self.services[service]['pretty'], payload['fqdn']))
-                resp = self.request('http://www.vpnhunter.com/poll', headers=headers, payload=payload)
-                if resp.status_code == 200:
-                    content = resp.json
-                    if len(content["result"]):
-                        for result in content["result"]:
-                            if len(result['address']) == 2:
-                                self.alert("Found 1 %s running %s on %s (port %s)" %
-                                           (self.services[service]['pretty'], result['vendor'], result['address'][0],
-                                            result['address'][1]))
-                            else:
-                                self.alert("Found 1 %s running %s on %s" %
-                                           (self.services[service]['pretty'], result['vendor'], result['address']))
-                else:
-                    self.error("An error occured while requesting vpnhunter.com")
