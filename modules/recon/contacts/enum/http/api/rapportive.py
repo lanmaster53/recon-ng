43e3b7a52f1dd853080745e039c89e40e4683bfc
LaNMaSteR53 <tjt1980@gmail.com>
2013-12-24 13:37:59 -0500

    Made major changes to the rapportive module. Mainly, added functionality which automatically handles the creation and maintenance of the session token required by the Rapportive API.

--- modules/recon/contacts/enum/http/api/rapportive.py
+++ modules/recon/contacts/enum/http/api/rapportive.py
@@ -9,53 +9,56 @@ class Module(framework.module):
         self.register_option('company', self.goptions['company']['value'], 'yes', self.goptions['company']['desc'])
         self.info = {
             'Name': 'Rapportive Contact Enumerator',
-            'Author': 'Quentin Kaiser (@qkaiser, contact[at]quentinkaiser.be)',
+            'Author': 'Quentin Kaiser (@qkaiser, contact[at]quentinkaiser.be) and Tim Tomes (@LaNMaSteR53)',
             'Description': 'Harvests contact information from the Rapportive.com API and updates the \'contacts\' table of the database with the results.',
             'Comments': []
         }
 
     def module_run(self):
 
-        resp = self.request('https://rapportive.com/login_status?user_email=%s@mail.com' % (self.random_str(15)))
-
-        if resp.status_code == 200 and 'session_token' in resp.json:
-            session_token = resp.json['session_token']
-            emails = self.get_source(self.options['source']['value'], "SELECT DISTINCT email FROM contacts ORDER BY email")
-            new = 0
-            for email in emails:
-                new += self.call_api(session_token, email)
-            if new: self.alert('NEW information found for %d contacts!' % (new))
-        else:
-            self.error('Error: %s' % (resp.json['error']))
-
-    def call_api(self, session_token, email):
-
+        emails = self.get_source(self.options['source']['value'], "SELECT DISTINCT email FROM contacts ORDER BY email")
+        session_token = self.get_rapportive_session_token()
         headers = {'X-Session-Token' : session_token}
-        resp = self.request('https://profiles.rapportive.com/contacts/email/%s' % (email), headers=headers)
-        found = 0
-        if resp.status_code == 200 and 'contact' in resp.json:
-            contact = resp.json['contact']
-            last_name = contact['last_name']
-            first_name = contact['first_name']
-            if any((first_name, last_name)):
-                self.output('%s => %s %s' % (email, first_name, last_name))
-                region = contact['location']
-                self.output('%s %s - %s' % (first_name, last_name, region))
-                for occupation in contact['occupations']:
-                    job_title = occupation['job_title']
-                    company = occupation['company']
-                    if self.options['company']['value'].lower() in company.lower():
-                        self.alert('%s %s - %s at %s' % (first_name, last_name, job_title, company))
-                        found = self.add_contact(first_name, last_name, job_title, email, region)
-                    else:
-                        self.output('%s %s - %s at %s' % (first_name, last_name, job_title, company))
-                for membership in contact['memberships']:
-                    site = membership['site_name']
-                    profile = membership['profile_url']
-                    self.output('%s %s - %s (%s)' % (first_name, last_name, site, profile))
-            else:
-                self.verbose('%s => Not found.' % (email))
-        else:
-            self.error('Error: %s' % (resp.json['error_code']))
+        cnt = 0
+        new = 0
+        email = emails.pop(0)
 
-        return found
+        while True:
+            resp = self.request('https://profiles.rapportive.com/contacts/email/%s' % (email), headers=headers)
+            if resp.status_code == 403:
+                # renew token
+                self.output('Renewing expired session token...')
+                self.delete_key('rapportive_token')
+                session_token = self.get_rapportive_session_token()
+                headers = {'X-Session-Token' : session_token}
+            elif resp.status_code == 200:
+                contact = resp.json['contact']
+                last_name = contact['last_name']
+                first_name = contact['first_name']
+                if any((first_name, last_name)):
+                    cnt += 1
+                    self.output('%s => %s %s' % (email, first_name, last_name))
+                    region = contact['location']
+                    self.output('%s %s - %s' % (first_name, last_name, region))
+                    for occupation in contact['occupations']:
+                        job_title = occupation['job_title']
+                        company = occupation['company']
+                        if self.options['company']['value'].lower() in company.lower():
+                            method = getattr(self, 'alert')
+                            new += self.add_contact(first_name, last_name, job_title, email, region)
+                        else:
+                            method = getattr(self, 'output')
+                        method('%s %s - %s at %s' % (first_name, last_name, job_title, company))
+                    for membership in contact['memberships']:
+                        site = membership['site_name']
+                        profile = membership['profile_url']
+                        self.output('%s %s - %s (%s)' % (first_name, last_name, site, profile))
+                else:
+                    self.verbose('%s => Not found.' % (email))
+            else:
+                self.error('Error: %s' % (resp.json['error_code']))
+            if not emails: break
+            email = emails.pop(0)
+
+        self.output('Information found for %d emails.' % (cnt))
+        if new: self.alert('NEW information found for %d contacts!' % (new))
