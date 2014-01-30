1a102aafdb6c83ffad246d618e03f6dfd66b687d
Steven Dumolt <dumolts@gmail.com>
2014-01-30 12:10:12 -0500

    Removed extra spaces

--- modules/recon/hosts/enum/http/api/virustotal.py
+++ modules/recon/hosts/enum/http/api/virustotal.py
@@ -13,7 +13,7 @@ class Module(framework.Module):
         framework.Module.__init__(self, params)        
         self.register_option('source', 'db', 'yes', 'Information to search results for')       
         self.register_option('sleep', '30', 'yes', 'Sleep after each system')  
-        self.info = {
+        self.info={
                      'Name': 'Virus Total Lookup',
                      'Author': 'Steven Dumolt (@dumolts)',
                      'Description': 'Uses Virus Total to gather data',
@@ -25,12 +25,12 @@ class Module(framework.Module):
                     }
 
     def _domain_search(self,str):
-        url = "https://www.virustotal.com/vtapi/v2/domain/report"
-        payload = {'domain': str,'apikey':self.api_key}        
-        resp = self.request(url, method="GET", payload=payload)
+        url="https://www.virustotal.com/vtapi/v2/domain/report"
+        payload={'domain': str,'apikey':self.api_key}        
+        resp=self.request(url, method="GET", payload=payload)
         
         if resp.json['response_code'] == 1:
-            tdata = []
+            tdata=[]
             
             for k in sorted(resp.json['resolutions']):
                 tdata.append([k['ip_address'],k['last_resolved']])
@@ -43,12 +43,12 @@ class Module(framework.Module):
             self.error(resp.json['verbose_msg'])
 
     def _ip_search(self,str):
-        url = "https://www.virustotal.com/vtapi/v2/ip-address/report"
-        payload = {'ip': str,'apikey':self.api_key}        
-        resp = self.request(url, method="GET", payload=payload)
+        url="https://www.virustotal.com/vtapi/v2/ip-address/report"
+        payload={'ip': str,'apikey':self.api_key}        
+        resp=self.request(url, method="GET", payload=payload)
         
         if resp.json['response_code'] == 1:
-            tdata = []
+            tdata=[]
             
             
             for k in sorted(resp.json['resolutions']):
@@ -60,12 +60,12 @@ class Module(framework.Module):
             self.error(resp.json['verbose_msg'])
 
     def _url_search(self,str):
-        url = "https://www.virustotal.com/vtapi/v2/url/report"
-        payload = {'resource': str,'apikey':self.api_key}
-        resp = self.request(url, method="POST", payload=payload)
+        url="https://www.virustotal.com/vtapi/v2/url/report"
+        payload={'resource': str,'apikey':self.api_key}
+        resp=self.request(url, method="POST", payload=payload)
 
         if resp.json['response_code'] == 1:
-            tdata = []
+            tdata=[]
             
             clean=[]
             for k in sorted(resp.json['scans']):
@@ -89,7 +89,7 @@ class Module(framework.Module):
         self.api_key=self.get_key('virustotal_api')
 
         
-        hosts = self.get_source(self.options['source'], 'SELECT DISTINCT host FROM hosts WHERE host IS NOT NULL ORDER BY host')
+        hosts=self.get_source(self.options['source'], 'SELECT DISTINCT host FROM hosts WHERE host IS NOT NULL ORDER BY host')
         
         for host in hosts:
             x=urlparse(host)
@@ -98,14 +98,14 @@ class Module(framework.Module):
             self._url_search(host)        
 
             if 'http' in x.scheme:
-                site = x.netloc
+                site=x.netloc
             elif not x.scheme:
-                site = x.path
+                site=x.path
             else:
                 site=x.scheme        
             
 
-            invalidChar = [':','/','\\']
+            invalidChar=[':','/','\\']
             if not any(invalid in site for invalid in invalidChar): 
                 if re.search(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$', site):
                     self._ip_search(site)            
