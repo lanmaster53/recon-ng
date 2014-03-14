8829fefbcf19207ecf0e55b853fc18215b8c4280
LaNMaSteR53 <tjt1980@gmail.com>
2014-03-14 00:26:32 -0400

    Added the facebook and jigsaw contact harvesting modules to the framework.

---
+++ modules/recon/contacts/gather/http/web/facebook.py
@@ -0,0 +1,113 @@
+import module
+# unique to module
+import json
+import re
+import urllib
+
+class Module(module.Module):
+
+    def __init__(self, params):
+        module.Module.__init__(self, params)
+        self.register_option('company', self.global_options['company'], 'yes', self.global_options.description['company'])
+        self.register_option('username', None, 'yes', 'Facebook account username')
+        self.register_option('password', None, 'yes', 'Facebook account password')
+        self.info = {
+                     'Name': 'Facebook Contact Enumerator',
+                     'Author': 'Quentin Kaiser (@qkaiser) and Tim Tomes (@LaNMaSteR53)',
+                     'Description': 'Harvests contacts from Facebook.com and updates the \'contacts\' table of the database with the results.',
+                     }
+
+    def module_run(self):
+        self.br = self.browser()
+        self.cnt = 0
+        self.new = 0
+        if self.login(self.options['username'],self.options['password']):
+            company = self.get_company_id(self.options['company'])
+            if company:
+                self.get_contacts(str(company))
+                self.output('%d total contacts found.' % (self.cnt))
+                if self.new: self.alert('%d NEW contacts found!' % (self.new))
+        else:
+            self.output('Authentication failed.')
+
+    def login(self, username, password):
+        self.verbose('Authenticating to Facebook...')
+        resp = self.br.open('https://www.facebook.com')
+        for form in self.br.forms():
+            if form.attrs['id'] == 'login_form':
+                self.br.form = form
+                break
+        self.br["email"] = username
+        self.br["pass"] = password
+        resp = self.br.submit()
+        return False if 'login_attempt' in str(resp.info()) else True
+
+    def get_company_id(self, company):
+        search = '"people who work for %s"' % (company)
+        self.verbose('Searching Facebook Graph API for: %s ...' % search)
+        search = urllib.quote(search)
+        resource = '/typeahead/search/facebar/query/?value=[%s]&grammar_version=9a4a4f16b0229da235ba203aa5cfa4d59acfa507&max_results=10&__a=1' % (search)
+        resp = self.br.open(resource)
+        content = resp.read()
+        companies = json.loads(content[9:])['payload']['entities']
+        # return nothing if there are no matches
+        if not companies:
+            self.output('No Company Matches Found.')
+            return
+        # return a unique match in such exists
+        elif len(companies) == 1:
+            self.alert('Unique Company Match Found: %s' % (companies[0]['text']))
+            return companies[0]['uid']
+        # prompt the user to choose from multiple matches
+        else:
+            # display the choices
+            choices = range(0, len(companies))
+            for i in choices:
+                self.output('[%d] %s' % (i, companies[i]['text']))
+            choice = raw_input('Choose a Company [0]: ')
+            # the first choice is the default
+            if choice is '':
+                return companies[0]['uid']
+            # make sure the choice is valid
+            elif int(choice) not in choices:
+                self.output('Invalid choice.')
+                return
+            # return the chosen company id
+            else:
+                return companies[int(choice)]['uid']
+
+    def get_contacts(self, company):
+        self.verbose('Extracting results for entity ID \'%s\'...' % (company))
+        entities = []
+        # get first results from the page itself
+        resp = self.br.open('/search/%s/employees/present' % (urllib.pathname2url(company)))
+        content = resp.read()
+        self.extract_entities(content)
+        # find bigpipe information
+        bigpipe = re.search(r'\{"view":"[^"]*","encoded_query":"([^"]*)","encoded_title":"([^"]*)","ref":"[^"]*","logger_source":"[^"]*","typeahead_sid":"[^"]*","tl_log":[^,]*,"impression_id":"[^"]*","filter_ids":\{[^\}]*\},"experience_type":"[^"]*","exclude_ids":[^\}]*\}', content)
+        if bigpipe:
+            query = bigpipe.group(1)
+            title = bigpipe.group(2)
+            while True:
+                cursor = re.search(r'\[\{"cursor":"([^"]*)","ads_at_end":[^\}]*\}\]', content)
+                if all((cursor, 'End of results' not in content, 'partial matches' not in content)):
+                    data = '{"view":"list","encoded_query":"%s","encoded_title":"%s","experience_type":"grammar","cursor":"%s","ads_at_end":true}' % (query, title, cursor.group(1))
+                    url = 'https://www.facebook.com/ajax/pagelet/generic.php/BrowseScrollingSetPagelet?data=%s&__a=1' % (urllib.quote(data))
+                    resp = self.br.open(url)
+                    content = resp.read()
+                    payload = json.loads(content[9:])['payload']
+                    self.extract_entities(payload)
+                else:
+                    break
+
+    def extract_entities(self, content):
+        names = re.findall(r'data-bt="[^"]*title[^"]*"><a href="[^"]*">([^<]*)', content)
+        titles = re.findall(r'data-bt="[^"]*sub_headers[^"]*">(?:<a href="[^"]*" data-hovercard="[^"]*">([^<]*)</a>)*', content)
+        if len(names) != len(titles):
+            self.alert('Inconsistent quantity of names and titles parsed. Data corruption imminent.')
+        for name, title in zip(names, titles):
+            fname, mname, lname = self.parse_name(name)
+            title = self.html_unescape(title or 'Employee')
+            self.output('%s - %s' % (name, title))
+            self.cnt += 1
+            self.new += self.add_contact(fname=fname, mname=mname, lname=lname, title=title)
---
+++ modules/recon/contacts/gather/http/web/jigsaw.py
@@ -0,0 +1,119 @@
+import module
+# unique to module
+import re
+import urllib
+
+class Module(module.Module):
+
+    def __init__(self, params):
+        module.Module.__init__(self, params)
+        self.register_option('company', self.global_options['company'], 'yes', self.global_options.description['company'])
+        self.register_option('company_url', None, 'no', 'direct url to the company page (skip discovery)')
+        self.info = {
+                     'Name': 'Jigsaw Contact Enumerator',
+                     'Description': 'Harvests contacts from Jigsaw.com and updates the \'contacts\' table of the database with the results.',
+                     'Comments': [
+                                  'Discovery does not always succeed due to alphabetical inconsistencies in the Data.com data sets. Use the following link to drill down to the target company and set the \'COMPANY_URL\' option.',
+                                  'Link: http://www.data.com/connect/index.jsp'
+                                  ]
+                     }
+
+    def module_run(self):
+        if self.options['company_url']:
+            self.get_contacts(self.options['company_url'])
+        else:
+            host = 'https://connect.data.com'
+            resource = self.get_company_url(host)
+            if resource:
+                self.get_contacts(host + resource)
+
+    def get_company_url(self, host):
+        self.output('Fetching Company URL...')
+        resource = '/directory/company/%s' % (self.options['company'][0].lower())
+        while True:
+            # widdle down through the alphabetical list of companies
+            url = host + resource
+            self.verbose('Query: %s' % (url))
+            resp = self.request(url)
+            # check to see if companies have been reached
+            if re.search('/directory/company/list', resp.text):
+                return self.choose_company(resp)
+            # keep widdling
+            tags = re.findall('[^>]<a href="(/directory/company/[^"]*)">([^<]*)</a>', resp.text)
+            # conduct an alphabetical comparison to determing which range to select
+            for tag in tags:
+                first = tag[1].split(' - ')[0].lower()
+                last = tag[1].split(' - ')[1].lower()
+                middle = self.options['company'].lower()
+                order = [first, middle, last]
+                ordered = sorted(order)
+                if order == ordered:
+                    resource = tag[0]
+                    self.output('Alphabetical range identified: %s' % (tag[1]))
+                    break
+            # resource should equal tag[0] if range was found, otherwise return nothing
+            if resource != tag[0]:
+                self.output('Company not found in the provided alphabetical ranges.')
+                return
+
+    def choose_company(self, resp):
+        companies = []
+        tags = re.findall('<a href="(/directory/company/list/[^"]*)">([^<]*)</a>', resp.text)
+        # build companies list of possible matches
+        for tag in tags:
+            if self.options['company'].lower() in tag[1].lower():
+                companies.append(tag)
+        # return nothing if there are no matches
+        if not companies:
+            self.output('No Company Matches Found.')
+            return
+        # return a unique match in such exists
+        elif len(companies) == 1:
+            self.alert('Unique Company Match Found: %s' % (companies[0][1]))
+            return companies[0][0]
+        # prompt the user to choose from multiple matches
+        else:
+            # display the choices
+            choices = range(0, len(companies))
+            for i in choices:
+                self.output('[%d] %s' % (i, companies[i][1]))
+            choice = raw_input('Choose a Company [0]: ')
+            # the first choice is the default
+            if choice is '':
+                return companies[0][0]
+            # make sure the choice is valid
+            elif int(choice) not in choices:
+                self.output('Invalid choice.')
+                return
+            # return the chosen company id
+            else:
+                return companies[int(choice)]['id']
+
+    def get_contacts(self, url):
+        self.output('Fetching Contacts...')
+        payload = {'page': 1}
+        cnt = 0
+        new = 0
+        while True:
+            self.verbose('Query: %s?%s' % (url, urllib.urlencode(payload)))
+            resp = self.request(url, payload=payload)
+            if 'Sorry. No contacts found for' in resp.text: break
+            #if not re.search('\?page='): break
+            values = re.findall('<td class="ellipsis">[^>]*>([^<]*)</span>', resp.text, re.DOTALL)
+            contacts = [values[i:i+6] for i in range(0, len(values), 6)]
+            for contact in contacts:
+                fname = self.html_unescape(contact[1])
+                lname = self.html_unescape(contact[0])
+                title = self.html_unescape(contact[2])
+                loc1 = self.html_unescape(contact[5])
+                loc2 = self.html_unescape('')
+                region = []
+                for item in [loc1, loc2]:
+                    if item: region.append(item)
+                region = ', '.join(region)
+                self.output('%s %s - %s (%s)' % (fname, lname, title, region))
+                cnt += 1
+                new += self.add_contact(fname=fname, lname=lname, title=title, region=region)
+            payload['page'] += 1
+        self.output('%d total contacts found.' % (cnt))
+        if new: self.alert('%d NEW contacts found!' % (new))
