beb12a8fba12051f3a7fb5bee4cdc4cd76b6d90d
LaNMaSteR53 <tjt1980@gmail.com>
2014-05-11 00:49:18 -0400

    Modified the bozocrack module to add Google error checking and shun avoidance.

--- modules/recon/creds/enum/http/web/bozocrack.py
+++ modules/recon/creds/enum/http/web/bozocrack.py
@@ -1,6 +1,8 @@
 import module
 # unique to module
 import hashlib
+import random
+import time
 
 class Module(module.Module):
 
@@ -19,7 +21,13 @@ class Module(module.Module):
         url = 'http://www.google.com/search'
         for hashstr in hashes:
             payload = {'q': hashstr}
-            resp = self.request(url, payload=payload)
+            resp = self.request(url, payload=payload, redirect=False)
+            if resp.status_code != 200:
+                if resp.status_code == 302:
+                    self.error('You\'ve been temporarily banned by Google for violating the terms of service.')
+                else:
+                    self.error('Google has encountered an error.')
+                break
             #re.sub('[\.:?]', ' ', resp.text).split()
             wordlist = set(resp.raw.replace('.', ' ').replace(':', ' ').replace('?', '').split(' '))
             plaintext, hashtype = crack(hashstr, wordlist)
@@ -28,6 +36,8 @@ class Module(module.Module):
                 self.query('UPDATE creds SET password=\'%s\', type=\'%s\' WHERE hash=\'%s\'' % (plaintext, hashtype, hashstr))
             else:
                 self.verbose('Value not found for hash: %s' % (hashstr))
+            # sleep to avoid lock-out
+            time.sleep(random.randint(3,5))
 
 def crack(hashstr, wordlist):
     for word in wordlist:
