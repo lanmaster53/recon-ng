179f55c1a9e0799e4e48511768f54579efe49f08
LaNMaSteR53 <tjt1980@gmail.com>
2014-06-03 23:58:05 -0400

    Optimized the list and csv reporting modules.

--- modules/reporting/csv.py
+++ modules/reporting/csv.py
@@ -6,33 +6,26 @@ class Module(module.Module):
 
     def __init__(self, params):
         module.Module.__init__(self, params)
-        self.register_option('filename', '%s/results.csv' % (self.workspace), 'yes', 'path and filename for report output')
-        self.register_option('tables', 'hosts, contacts, creds', 'yes', 'comma delineated list of tables')
+        self.register_option('filename', '%s/results.csv' % (self.workspace), 'yes', 'path and filename for output')
+        self.register_option('table', 'hosts', 'yes', 'source table of data to export')
         self.info = {
                      'Name': 'CSV File Creator',
                      'Author': 'Tim Tomes (@LaNMaSteR53)',
-                     'Description': 'Creates a CSV file containing the specified harvested data types.',
+                     'Description': 'Creates a CSV file containing the specified harvested data.'
                      }
 
     def module_run(self):
-        # validate that the file can be created
         filename = self.options['filename']
         # codecs module not used because the csv module converts to ascii
-        outfile = open(filename, 'w')
-
-        # build a list of table names
-        tables = [x.strip() for x in self.options['tables'].split(',')]
-
-        rows = []
-        for table in tables:
-            rows.extend(self.query('SELECT * FROM "%s" ORDER BY 1' % (table)))
-        cnt = 0
-        for row in rows:
-            row = [x if x else '' for x in row]
-            if any(row):
-                cnt += 1
-                csvwriter = csv.writer(outfile, quoting=csv.QUOTE_ALL)
-                csvwriter.writerow([s.encode("utf-8") for s in row])
-        outfile.close()
-
+        with open(filename, 'w') as outfile:
+            # build a list of table names
+            table = self.options['table']
+            rows = self.query('SELECT * FROM "%s" ORDER BY 1' % (table))
+            cnt = 0
+            for row in rows:
+                row = [x if x else '' for x in row]
+                if any(row):
+                    cnt += 1
+                    csvwriter = csv.writer(outfile, quoting=csv.QUOTE_ALL)
+                    csvwriter.writerow([s.encode("utf-8") for s in row])
         self.output('%d records added to \'%s\'.' % (cnt, filename))
--- modules/reporting/list.py
+++ modules/reporting/list.py
@@ -6,32 +6,30 @@ class Module(module.Module):
 
     def __init__(self, params):
         module.Module.__init__(self, params)
-        self.register_option('table', 'hosts', 'yes', 'source table of the data for the list')
-        self.register_option('column', 'ip_address', 'yes', 'source column of the data for the list')
+        self.register_option('table', 'hosts', 'yes', 'source table of data for the list')
+        self.register_option('column', 'ip_address', 'yes', 'source column of data for the list')
         self.register_option('unique', True, 'yes', 'only return unique items from the dataset')
         self.register_option('nulls', False, 'yes', 'include nulls in the dataset')
         self.register_option('filename', '%s/list.txt' % (self.workspace), 'yes', 'path and filename for output')
         self.info = {
                      'Name': 'List Creator',
                      'Author': 'Tim Tomes (@LaNMaSteR53)',
-                     'Description': 'Creates a file containing a list of records from the database.',
+                     'Description': 'Creates a file containing a list of records from the database.'
                      }
 
     def module_run(self):
-        # validate that file can be created
         filename = self.options['filename']
-        outfile = codecs.open(filename, 'wb', encoding='utf-8')
-        # handle the source of information for the report
-        column = self.options['column']
-        table = self.options['table']
-        nulls = ' WHERE "%s" IS NOT NULL' % (column) if not self.options['nulls'] else ''
-        unique = 'DISTINCT ' if self.options['unique'] else ''
-        values = (unique, column, table, nulls)
-        query = 'SELECT %s"%s" FROM "%s"%s ORDER BY 1' % values
-        rows = self.query(query)
-        for row in [x[0] for x in rows]:
-            row = row if row else ''
-            outfile.write('%s\n' % (row))
-            print(row)
-        outfile.close()
+        with codecs.open(filename, 'wb', encoding='utf-8') as outfile:
+            # handle the source of information for the report
+            column = self.options['column']
+            table = self.options['table']
+            nulls = ' WHERE "%s" IS NOT NULL' % (column) if not self.options['nulls'] else ''
+            unique = 'DISTINCT ' if self.options['unique'] else ''
+            values = (unique, column, table, nulls)
+            query = 'SELECT %s"%s" FROM "%s"%s ORDER BY 1' % values
+            rows = self.query(query)
+            for row in [x[0] for x in rows]:
+                row = row if row else ''
+                outfile.write('%s\n' % (row))
+                print(row)
         self.output('%d items added to \'%s\'.' % (len(rows), filename))
